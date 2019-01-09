711117957e73886e749740843101a99a4a16fed1
Ashish Sharma <accssharma@gmail.com>
2019-01-08 20:31:33 -0700

    emacs: configure MELPA to work on ubuntu18.04

--- emacs.d/.emacs.d/lisp/init-elpa.el
+++ emacs.d/.emacs.d/lisp/init-elpa.el
@@ -17,7 +17,7 @@
 
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
+       (proto (if no-ssl "http" "http")))
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   ;; Official MELPA Mirror, in case necessary.
   ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
@@ -26,7 +26,7 @@
       (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))
     (unless no-ssl
       ;; Force SSL for GNU ELPA
-      (setcdr (assoc "gnu" package-archives) "https://elpa.gnu.org/packages/"))))
+      (setcdr (assoc "gnu" package-archives) "http://elpa.gnu.org/packages/"))))
 
 
 ;;; On-demand installation of packages
